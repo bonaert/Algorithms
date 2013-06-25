ca76bef082b0c52114b1a66de692a32e4722d72f
harokb <harokb@gmail.com>
2013-06-25 12:36:53 +0100

    Added algorithms to detect cycle in a graph.

--- Algorithms/Graph/ConnectedComponents.java
+++ Algorithms/Graph/ConnectedComponents.java
@@ -29,6 +29,47 @@ public class ConnectedComponents {
         while (!verticesToVisit.isEmpty()) {
             int vertex = verticesToVisit.dequeue();
             if (!marked[vertex]) {
+
+                for (Integer neighbor : graph.adj(vertex)) {
+                    if (!marked[neighbor]) verticesToVisit.enqueue(neighbor);
+                }
+
+                marked[vertex] = true;
+                id[vertex] = count;
+                size[count]++;
+            }
+        }
+    }
+
+    public ConnectedComponents(DirectedGraph graph) {
+
+        marked = new boolean[graph.vertices()];
+        id = new int[graph.vertices()];
+        size = new int[graph.vertices()];
+        count = 0;
+
+
+        for (int i = 0; i < graph.vertices(); i++) {
+            if (!marked[i]) {
+                breadthFirstSearch(graph, i);
+                count++;
+            }
+        }
+    }
+
+    private void breadthFirstSearch(DirectedGraph graph, int source) {
+
+        Queue<Integer> verticesToVisit = new Queue<Integer>();
+        verticesToVisit.enqueue(source);
+
+        while (!verticesToVisit.isEmpty()) {
+            int vertex = verticesToVisit.dequeue();
+            if (!marked[vertex]) {
+
+                for (Integer neighbor : graph.adj(vertex)) {
+                    if (!marked[neighbor]) verticesToVisit.enqueue(neighbor);
+                }
+
                 marked[vertex] = true;
                 id[vertex] = count;
                 size[count]++;
