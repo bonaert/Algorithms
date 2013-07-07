7cf674e5c758fce61e12321569e7db3c9172a3ca
harokb <harokb@gmail.com>
2013-07-07 19:51:01 +0200

    Added an algorithm to find the shortest path in a acyclic path.

---
+++ Algorithm/Algorithms/Graphs/AcyclicShortestPath.java
@@ -0,0 +1,91 @@
+package Graphs;
+
+import DataStructure.Stack;
+
+import java.util.Arrays;
+
+public class AcyclicShortestPath {
+
+    private double[] distTo;
+    private int[] edgeTo;
+
+    /**
+     * Finds the shortest path from a source
+     * to every reachable vertex in a edge weighted
+     * directed graph.
+     * <p/>
+     * Warning: the graph must be acyclic.
+     *
+     * @param graph
+     * @param source
+     */
+    public AcyclicShortestPath(EdgeWeightedDirectedGraph graph, int source) {
+
+        TopologicalOrder order = new TopologicalOrder(graph);
+
+        distTo = new double[graph.vertices()];
+        Arrays.fill(distTo, Double.POSITIVE_INFINITY);
+        distTo[source] = 0.0;
+
+        edgeTo = new int[graph.vertices()];
+
+        for (Integer vertex : order.topologicalOrder()) {
+            for (DirectedEdge edge : graph.adjEdges(vertex)) {
+                relax(edge);
+            }
+        }
+
+    }
+
+    private void relax(DirectedEdge edge) {
+        int from = edge.source();
+        int to = edge.dest();
+        if (distTo[to] > edge.weight() + distTo[from]) {
+            distTo[to] = edge.weight() + distTo[from];
+            edgeTo[to] = from;
+        }
+    }
+
+    /**
+     * Returns true if there is a path from the source
+     * to the vertex. Otherwise, returns false.
+     *
+     * @param vertex
+     */
+    public boolean hasPathTo(int vertex) {
+        return distTo[vertex] != Double.POSITIVE_INFINITY;
+    }
+
+    /**
+     * Returns the distance from the source to
+     * the given vertex. If the vertex is
+     * unreachable, the distance returned is
+     * Double.POSITIVE_INFINITY.
+     *
+     * @param vertex
+     */
+    public double distTo(int vertex) {
+        return distTo[vertex];
+    }
+
+    /**
+     * Returns the edges from the source to the given vertex,
+     * is such a path exists. Otherwise, it returns null.
+     *
+     * @param vertex
+     */
+    public Iterable<Integer> pathTo(int vertex) {
+        if (!hasPathTo(vertex)) return null;
+
+        Stack<Integer> edges = new Stack<Integer>();
+
+        int node;
+        for (node = vertex; distTo[node] != 0; node = edgeTo[node]) {
+            edges.push(node);
+        }
+
+        edges.push(node);
+        return edges;
+    }
+
+}
