0d70093404ba70b4e128a978c1aa3cd82858c64f
harokb <harokb@gmail.com>
2013-08-07 17:11:46 +0100

    Refactored (made smaller functions.)

--- Algorithm/Algorithms/Graphs/SymbolGraph.java
+++ Algorithm/Algorithms/Graphs/SymbolGraph.java
@@ -6,42 +6,67 @@ import java.util.Scanner;
 
 public class SymbolGraph {
 
-    private String keys[];
+    private String indexToName[];
     private Map<String, Integer> keysToIndex;
     private UndirectedGraph graph;
 
 
     public SymbolGraph(String filename, String delimiter) {
 
-        Scanner scanner = new Scanner(filename);
-        keysToIndex = new HashMap<String, Integer>();
-
         // First pass: Find all different vertices
-        while (scanner.hasNext()) {
+        keysToIndex = buildNameToIndexMapping(filename, delimiter);
 
+        // Second pass: build index -> name mapping
+        indexToName = buildIndexToNameMapping();
+
+        // Third pass : build graph
+        graph = buildGraph(filename, delimiter);
+    }
+
+    private Map<String, Integer> buildNameToIndexMapping(String filename, String delimiter) {
+        Map<String, Integer> keysToIndex = new HashMap<String, Integer>();
+
+        Scanner scanner = new Scanner(filename);
+        while (scanner.hasNext()) {
             String[] vertices = scanner.next().split(delimiter);
-            for (String vertex : vertices) {
-                if (!keysToIndex.containsKey(vertex)) {
-                    keysToIndex.put(vertex, keysToIndex.size());
-                }
-            }
+            addVerticesToMapping(keysToIndex, vertices);
         }
 
-        // Second pass: build index -> key mapping
-        keys = new String[keysToIndex.size()];
+        return keysToIndex;
+    }
+
+    private void addVerticesToMapping(Map<String, Integer> keysToIndex, String[] vertices) {
+        for (String vertex : vertices) {
+            if (!keysToIndex.containsKey(vertex)) {
+                keysToIndex.put(vertex, keysToIndex.size());
+            }
+        }
+    }
+
+    private String[] buildIndexToNameMapping() {
+        String[] keys = new String[keysToIndex.size()];
         for (String vertex : keysToIndex.keySet()) {
             keys[keysToIndex.get(vertex)] = vertex;
         }
+        return keys;
+    }
 
-        // Third pass : build graph
-        graph = new UndirectedGraph(keys.length);
-        scanner = new Scanner(filename);
+    private UndirectedGraph buildGraph(String filename, String delimiter) {
+        UndirectedGraph graph = new UndirectedGraph(indexToName.length);
+
+        Scanner scanner = new Scanner(filename);
+
         while (scanner.hasNext()) {
             String[] vertices = scanner.next().split(delimiter);
-            int sourceIndex = keysToIndex.get(vertices[0]);
-            for (int i = 1; i < vertices.length; i++) {
-                graph.addEdge(sourceIndex, keysToIndex.get(vertices[i]));
-            }
+            addEdges(graph, vertices);
+        }
+        return graph;
+    }
+
+    private void addEdges(UndirectedGraph graph, String[] vertices) {
+        int sourceIndex = keysToIndex.get(vertices[0]);
+        for (int i = 1; i < vertices.length; i++) {
+            graph.addEdge(sourceIndex, keysToIndex.get(vertices[i]));
         }
     }
 
@@ -50,7 +75,7 @@ public class SymbolGraph {
     }
 
     public String name(int index) {
-        return keys[index];
+        return indexToName[index];
     }
 
     public int index(String name) {
