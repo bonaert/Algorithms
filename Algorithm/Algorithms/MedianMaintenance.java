a4a8ef9a61be56892c4f7cffbcf87e59c0c28ef6
harokb <harokb@gmail.com>
2013-08-12 18:35:33 +0100

    Simplified code a bit.

--- Algorithm/Algorithms/MedianMaintenance.java
+++ Algorithm/Algorithms/MedianMaintenance.java
@@ -14,20 +14,14 @@ public class MedianMaintenance {
         heapHigh = new MinimumPriorityQueue<Integer>();
 
         In in = new In(filepath);
-        int i = 0;
         while (!in.isEmpty()) {
             int number = in.readInt();
             insertElement(number);
-            i++;
             sum += getMedian();
-            if ((i & (i - 1)) == 0) {
-                System.out.println(i + " = " + getMedian() + " (" + sum + ")");
-            }
-
         }
     }
 
-    public MedianMaintenance(int[] numbers){
+    public MedianMaintenance(int[] numbers) {
 
         heapLow = new MaximumPriorityQueue<Integer>();
         heapHigh = new MinimumPriorityQueue<Integer>();
@@ -35,24 +29,7 @@ public class MedianMaintenance {
         for (int number : numbers) {
             insertElement(number);
             sum += getMedian();
-            printHeaps();
         }
-    }
-
-    public void printHeaps(){
-        for (Integer number : heapLow) {
-            System.out.print(number + " ");
-        }
-
-        System.out.println();
-
-        for (Integer number : heapHigh) {
-            System.out.print(number + " ");
-
-        }
-
-        System.out.println();
-        System.out.println();
     }
 
     private void insertElement(int number) {
@@ -78,26 +55,14 @@ public class MedianMaintenance {
     }
 
     private int getMedian() {
-
-        int medianIndex = (numberElements % 2 == 0) ? (numberElements / 2) : ((numberElements + 1) / 2);
-
-        if (medianIndex == heapLow.size()){
-            return heapLow.max();
-        } else if (medianIndex == heapLow.size() + 1){
-            throw new Error();
-            //return heapHigh.min();
-        } else {
-            throw new Error();
-        }
-
-
+        return heapLow.max();
     }
 
     public int getSum() {
         return sum;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String filepath = "/home/greg/Desktop/median.txt";
 
         Stopwatch stopwatch = new Stopwatch();
