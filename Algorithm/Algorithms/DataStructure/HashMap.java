e0e67badd37efd4f8fcd65dfdf12c0c52dcba0c9
harokb <harokb@gmail.com>
2013-12-22 22:40:19 +0100

    Fixed hashmap implementation and added testing.

--- Algorithm/Algorithms/DataStructure/HashMap.java
+++ Algorithm/Algorithms/DataStructure/HashMap.java
@@ -1,65 +1,76 @@
-/*
+
 package DataStructure;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class HashMap<Key, Value> {
 
-    class Entry<Key, Value> {
+    static class Entry<Key, Value> implements Map.Entry<Key, Value> {
 
-        private Key key;
-        private Value value;
-        private Entry<Key, Value> nextEntry;
+        final Key key;
+        Value value;
+        Entry<Key, Value> nextEntry;
         int hash;
 
-        private Entry(Key key, Value value, Entry nextEntry) {
+        Entry(int hash, Key key, Value value, Entry<Key, Value> nextEntry) {
             this.key = key;
             this.value = value;
             this.nextEntry = nextEntry;
+            this.hash = hash;
         }
 
-        public Key getKey() {
+        public final Key getKey() {
             return key;
         }
 
-        public Value getValue() {
+        public final Value getValue() {
             return value;
         }
 
-        public final Entry nextEntry() {
-            return nextEntry;
+        public Value setValue(Value value) {
+            Value oldValue = this.value;
+            this.value = value;
+            return oldValue;
         }
 
-        private void setNextEntry(Entry nextEntry) {
-            this.nextEntry = nextEntry;
+        public boolean isLastEntryInChain() {
+            return nextEntry == null;
+        }
+
+        public boolean hasKey(Key key) {
+            return key.equals(this.key);
+        }
+
+        public boolean hasValue(Value value) {
+            return value.equals(this.value);
         }
     }
 
     private int capacity;
-    private Entry[] entries;
-    private int size;
-    private final double loadFactor;
+    transient Entry<Key, Value>[] entries;
+    transient int size;
+    final double loadFactor;
 
 
     static final double DEFAULT_LOAD_CAPACITY = 0.75;
     static final int DEFAULT_INITIAL_CAPACITY = 16;
 
 
-    */
-/**
+    /**
      * Creates a hash map, that is a map that
      * associates a key with a value, but with very fast
      * insertion, deletion, getting and check if the
      * key is contained, provided that the hash
      * function of the key is uniformly distributed.
-     *//*
+     */
 
     public HashMap() {
         this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_CAPACITY);
     }
 
-    */
-/**
+
+    /**
      * Creates a hash map, that is a map that
      * associates a key with a value, but with very fast
      * insertion, deletion, getting and check if the
@@ -67,14 +78,14 @@ public class HashMap<Key, Value> {
      * function of the key is uniformly distributed.
      *
      * @param capacity
-     *//*
+     */
 
     public HashMap(int capacity) {
         this(capacity, DEFAULT_LOAD_CAPACITY);
     }
 
-    */
-/**
+
+    /**
      * Creates a hash map, that is a map that
      * associates a key with a value, but with very fast
      * insertion, deletion, getting and check if the
@@ -83,141 +94,133 @@ public class HashMap<Key, Value> {
      *
      * @param initialCapacity
      * @param loadFactor
-     *//*
+     */
 
     public HashMap(int initialCapacity, double loadFactor) {
         size = 0;
 
         // Find a power of 2 >= initialCapacity
-        int possibleCapacity = 1;
-        while (possibleCapacity < initialCapacity)
-            possibleCapacity <<= 1;
+        int capacity = 1;
+        while (capacity < initialCapacity) capacity *= 2;
 
-        this.capacity = possibleCapacity;
-
-        entries = (Entry[]) new Object[capacity];
+        this.capacity = capacity;
         this.loadFactor = loadFactor;
+
+        entries = new Entry[this.capacity];
     }
 
-    */
-/**
+
+    /**
      * Returns the value associated with this key
      * in the hash map.
      *
      * @param key
-     *//*
-
+     */
     public Value get(Key key) {
-
         int hash = computeHash(key);
-        Entry entry = entries[hash];
+        Entry<Key, Value> entry = entries[hash];
 
-        while (entry != null && !entry.getKey().equals(key)) {
-            entry = entry.nextEntry();
+        while (entry != null && !entry.hasKey(key)) {
+            entry = entry.nextEntry;
         }
 
-        if (entry != null) return entry.getValue();
-        return null;
+        return (entry == null) ? null : entry.getValue();
     }
 
-    */
-/**
+
+    /**
      * Puts the given key and value in the hash map.
      *
      * @param key
      * @param value
-     *//*
-
+     */
     public void put(Key key, Value value) {
-
         if (size >= capacity * loadFactor) resize(2 * capacity);
 
         int hash = computeHash(key);
-        Entry entry = entries[hash];
-
-        Entry newEntry = new Entry(key, value, null);
-
-        if (isEntry(hash)) {
-            while (entry.nextEntry != null) {
-                entry = entry.nextEntry;
-            }
-            entry.setNextEntry(newEntry);
-            entry = entry.nextEntry();
-        }
-
-        entry = newEntry;
-        size++;
+        addEntry(key, value, hash);
     }
 
     private void resize(int newSize) {
-        Entry[] oldEntries = entries;
-        entries = (Entry[]) new Object[newSize];
-
+        System.out.println("Resizing");
+        Entry<Key, Value>[] oldEntries = entries;
+        entries = new Entry[newSize];
 
         capacity = newSize;
+        size = 0;
 
-        for (Entry oldEntry : oldEntries) {
-
-            add(entries, oldEntry);
-
-            oldEntry = oldEntry.nextEntry();
-            while (oldEntry != null) {
-                add(entries, oldEntry);
-            }
+        for (Entry<Key, Value> oldFirstEntryInChain : oldEntries) {
+            addAllEntriesWithSameHash(oldFirstEntryInChain);
         }
     }
 
-    private void add(Entry[] newEntries, Entry newEntry) {
 
-        Key key = (Key) newEntry.getKey();
-        Value value = (Value) newEntry.getValue();
+    private void addAllEntriesWithSameHash(Entry<Key, Value> firstEntryInChain) {
+        for (Entry<Key, Value> entry = firstEntryInChain; entry != null; entry = entry.nextEntry) {
+            addEntry(entry);
+        }
+    }
 
-        int hash = computeHash(key);
-        Entry entry = entries[hash];
+    private void addEntry(Entry<Key, Value> entry) {
+        int hash = entry.hash;
+        Key key = entry.getKey();
+        Value value = entry.getValue();
+        addEntry(key, value, hash);
+    }
 
-        Entry nextEntry = new Entry(key, value, null);
+    private void addEntry(Key key, Value value, int hash) {
+        Entry<Key, Value> newEntry = new Entry<Key, Value>(hash, key, value, null);
 
-        if (isEntry(hash)) {
-            while (entry.nextEntry != null) {
-                entry = entry.nextEntry;
-            }
-            entry.setNextEntry(nextEntry);
-            entry = entry.nextEntry();
+        if (entryExists(hash)) {
+            Entry<Key, Value> lastEntry = getLastEntry(hash);
+            lastEntry.nextEntry = newEntry;
+        } else {
+            entries[hash] = newEntry;
         }
 
-        entry = nextEntry;
         size++;
     }
 
-    */
-/**
+    private Entry<Key, Value> getLastEntry(int hash) {
+        Entry<Key, Value> entry = entries[hash];
+        while (!entry.isLastEntryInChain()) {
+            entry = entry.nextEntry;
+        }
+        return entry;
+    }
+
+
+    /**
      * Remove the key and value in the hash map.
      * If the key is not present, this methods doesn't
      * do anything.
-     *//*
-
+     */
     public void remove(Key key) {
-
         int hash = computeHash(key);
-        Entry entry = entries[hash];
 
-        if (entry == null) return;
+        if (!entryExists(hash)) {
+            return;
+        }
 
-        if (entry.getKey().equals(key)) {
-            entries[hash] = entry.nextEntry();
+        Entry<Key, Value> entry = entries[hash];
+
+        // [Good Entry] -> [A] -> ... transforms into [A] -> ...
+        if (entry.hasKey(key)) {
+            entries[hash] = entry.nextEntry;
             size--;
             return;
         }
 
-        while (entry.nextEntry != null) {
-
-            if (entry.nextEntry.getKey().equals(key)) {
-                entry = entry.nextEntry().nextEntry();
+        // ... -> [A] -> [Good Entry] -> [B]  transform into ... -> [A] -> [B]
+        while (!entry.isLastEntryInChain()) {
+            Entry<Key, Value> nextEntry = entry.nextEntry;
+            if (nextEntry.hasKey(key)) {
+                entry.nextEntry = (nextEntry.isLastEntryInChain()) ? null : nextEntry.nextEntry;
                 size--;
                 return;
             }
+            entry = entry.nextEntry;
         }
-
     }
 
     public void clear() {
@@ -225,37 +228,40 @@ public class HashMap<Key, Value> {
         size = 0;
     }
 
-    */
-/**
+
+    /**
      * Returns true if the key is in the hash map,
      * otherwise returns false;
-     *//*
-
+     */
     public boolean containsKey(Key key) {
+        if (key == null) throw new IllegalArgumentException("Null cannot be a key!");
+
         int hash = computeHash(key);
+        if (!entryExists(hash)) return false;
 
-        Entry entry = entries[hash];
-        if (entry == null) return false;
+        Entry<Key, Value> entry = entries[hash];
 
-        while (entry != null && !entry.getKey().equals(key)) {
-            entry = entry.nextEntry();
+        while (entry != null) {
+            if (entry.hasKey(key)) return true;
+            entry = entry.nextEntry;
         }
 
-        return (entry != null);
+        return false;
     }
 
-    */
-/**
+
+    /**
      * Returns true if the value is in the hash map,
      * otherwise returns false;
-     *//*
+     */
 
     public boolean containsValue(Value value) {
+        if (value == null) throw new IllegalArgumentException("Null cannot be a key!");
 
-        for (int i = 0; i < capacity; i++) {
-
-            for (Entry entry = entries[i]; entry != null; entry = entry.nextEntry()) {
-                if (entry.getValue().equals(value)) return true;
+        for (Entry<Key, Value> entry : entries) {
+            while (entry != null) {
+                if (entry.hasValue(value)) return true;
+                entry = entry.nextEntry;
             }
         }
 
@@ -266,24 +272,24 @@ public class HashMap<Key, Value> {
         return (key.hashCode() & 0x7FFFFFF) % capacity;
     }
 
-    private boolean isEntry(int hash) {
+    private boolean entryExists(int hash) {
         return entries[hash] != null;
     }
 
-    */
-/**
+
+    /**
      * Returns true if the hash map is empty,
      * otherwise return false.
-     *//*
+     */
 
     public boolean isEmpty() {
         return (size == 0);
     }
 
-    */
-/**
+
+    /**
      * Returns the size of the hash map.
-     *//*
+     */
 
     public int size() {
         return size;
@@ -301,10 +307,7 @@ public class HashMap<Key, Value> {
                 throw new Exception();
             }
         }
-
-
     }
 
-
 }
-*/
+
---
+++ Algorithm/Algorithms/DataStructure/HashMapTest.java
@@ -0,0 +1,142 @@
+package DataStructure;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+public class HashMapTest {
+    private HashMap<Integer, Integer> hashMap;
+
+    @Before
+    public void setUp() throws Exception {
+        hashMap = new HashMap<Integer, Integer>();
+    }
+
+    @Test
+    public void emptyHashMapHasSizeZero_AndIsEmpty() {
+        assertSizeIsEqualTo(0);
+        assertIsEmpty();
+    }
+
+    @Test
+    public void canInsertOneElementInHashMap_IsNotEmptyAndSizeIsOne() {
+        hashMap.put(1, 2);
+        assertSizeIsEqualTo(1);
+        assertIsNotEmpty();
+    }
+
+    @Test
+    public void insertOneElement_ContainsKeyAndContainsValueWork() {
+        hashMap.put(1, 2);
+
+        assertContainsKey(1);
+        assertDoesNotContainKey(0);
+        assertDoesNotContainKey(2);
+
+        assertContainsValue(2);
+        assertDoesNotContainValue(1);
+        assertDoesNotContainValue(3);
+    }
+
+    @Test
+    public void canPutVariousElementWithDifferentKeys() {
+        hashMap.put(1, 2);
+        hashMap.put(2, 3);
+        hashMap.put(5, 10);
+
+        assertIsNotEmpty();
+        assertSizeIsEqualTo(3);
+    }
+
+    @Test
+    public void canPutVariousElementWithSameKeys() {
+        hashMap.put(1, 2);
+        hashMap.put(1, 5);
+        hashMap.put(1, 7);
+        hashMap.put(0, 3);
+
+        assertIsNotEmpty();
+        assertSizeIsEqualTo(4);
+    }
+
+    @Test
+    public void canGetElementAfterInsertingItInHashmap() {
+        hashMap.put(1, 5);
+
+        assertIsNotEmpty();
+        assertSizeIsEqualTo(1);
+        assertKeyValuePairIs(1, 5);
+        assertEquals(null, hashMap.get(2));
+    }
+
+    @Test
+    public void addTwoElementsAndRemoveOne_ReturnsGoodElementAndSize() {
+        hashMap.put(1, 2);
+        hashMap.put(5, 7);
+        hashMap.remove(1);
+
+        assertSizeIsEqualTo(1);
+        assertIsNotEmpty();
+
+        assertKeyValuePairIs(5, 7);
+        assertNull(hashMap.get(1));
+
+        assertDoesNotContainKey(1);
+        assertContainsKey(5);
+
+        assertContainsValue(7);
+        assertDoesNotContainValue(2);
+    }
+
+    @Test
+    public void canAddElementsAndClear_SizeGetContainsMethodsWork() {
+        hashMap.put(2, 3);
+        hashMap.put(4, 5);
+        hashMap.put(6, 7);
+        hashMap.clear();
+
+        assertIsEmpty();
+        assertSizeIsEqualTo(0);
+
+        assertDoesNotContainKey(2);
+        assertDoesNotContainKey(4);
+        assertDoesNotContainKey(6);
+
+        assertDoesNotContainValue(3);
+        assertDoesNotContainValue(5);
+        assertDoesNotContainValue(7);
+    }
+
+    private void assertDoesNotContainKey(int key) {
+        assertFalse(hashMap.containsKey(key));
+    }
+
+    private void assertContainsKey(int k) {
+        assertTrue(hashMap.containsKey(k));
+    }
+
+    private void assertDoesNotContainValue(int value) {
+        assertFalse(hashMap.containsValue(value));
+    }
+
+    private void assertIsNotEmpty() {
+        assertFalse(hashMap.isEmpty());
+    }
+
+    private void assertIsEmpty() {
+        assertTrue(hashMap.isEmpty());
+    }
+
+    private void assertContainsValue(int value) {
+        assertTrue(hashMap.containsValue(value));
+    }
+
+    private void assertKeyValuePairIs(int key, int value) {
+        assertEquals((Integer) value, hashMap.get(key));
+    }
+
+    private void assertSizeIsEqualTo(int size) {
+        assertEquals(size, hashMap.size());
+    }
+}
